f653c0f7233ba0d4acf8c62c563a87e595fc013f
Elizabeth Dowling <dowlinge94@gmail.com>
2022-03-21 10:01:29 -0400

    test 1, 3 passing

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 // Write your solution in this file!
-var customerName=  'bob'
+var customerName =  'bob'
+
+// function upperCaseCustomerName() {
+//     return (customerName.toUpperCase())
+// }
+// upperCaseCustomerName(customerName)
+// console.log(customerName.toUpperCase())
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+    return bestCustomer
+}
+
+function overwriteBestCustomer
